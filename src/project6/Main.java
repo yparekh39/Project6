017d117f8ccc3030e1b59e94135c993a474e888d
Matt Owens <mgowens@utexas.edu>
2015-12-03 20:50:00 -0600

    blackpegcount fix

--- src/project6/Main.java
+++ src/project6/Main.java
@@ -7,10 +7,12 @@ public class Main {
 		PegCombination answer = AIController.generateRandomPegCombination();
 		MastermindController.setAnswer(answer);
 		System.out.println(MastermindModel.answer.toString());
+		
 		while(MastermindModel.blackPegCount < 4 && MastermindModel.currentTurn < 12){
+			MastermindModel.blackPegCount = 0; //resets black peg count so it only checks for count this turn
 			MastermindController.takeTurn();
 		}
-		//viewConsole.printBoard();
+
 	}
 
 }
--- src/project6/viewConsole.java
+++ src/project6/viewConsole.java
@@ -5,12 +5,20 @@ public class viewConsole {
 	//print the board
 	public static void printBoard(){
 		//print each turn/row
-		for(int i = 0; i < MastermindModel.currentTurn; i++){
+		for(int i = 0; i < 12; i++){
 			//assemble and print a row
 			String turn = "";
-			turn += MastermindModel.GameState[i].pegCombination.toString();
+			if(MastermindModel.GameState[i] == null)
+				turn+="----";
+			else
+				turn += MastermindModel.GameState[i].pegCombination.toString();
+			
 			turn+="..........";
-			turn += MastermindModel.GameState[i].pegResponse.toString();
+			
+			if(MastermindModel.GameState[i] == null)
+				turn += "----";
+			else
+				turn += MastermindModel.GameState[i].pegResponse.toString();
 			System.out.println(turn);
 		}
 	}
